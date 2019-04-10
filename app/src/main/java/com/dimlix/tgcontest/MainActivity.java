e4baf32134eeba15c2f90cfc9392f817cfc7c181
Ryazantsev Dmitry <dilix90@gmail.com>
2019-04-11 01:29:05 +0300

    Fix colors, remove fake data

--- app/src/main/java/com/dimlix/tgcontest/MainActivity.java
+++ app/src/main/java/com/dimlix/tgcontest/MainActivity.java
@@ -17,7 +17,6 @@ import android.widget.ScrollView;
 
 import com.dimlix.tgcontest.chart.ChartLayout;
 import com.dimlix.tgcontest.model.ChartData;
-import com.dimlix.tgcontest.model.GraphData;
 
 import org.json.JSONException;
 
@@ -62,63 +61,6 @@ public class MainActivity extends AppCompatActivity {
         addChartFromFile("graph_3.json", "Fruits");
         addChartFromFile("graph_4.json", "Views");
         addChartFromFile("graph_5.json", "Fruits");
-
-
-
-
-
-        // TODO persist data across activity recreations, make graphData parcable
-        GraphData graphData = new JsonGraphReader().getGraphDataFromJson(loadGraphJSONFromAsset());
-        LayoutInflater inflater = LayoutInflater.from(this);
-        ViewGroup container = findViewById(R.id.container);
-        for (int i = 0; i < graphData.getChartData().size(); i++) {
-            ChartLayout chartView = (ChartLayout) inflater.inflate(R.layout.item_chart, container, false);
-            // Need to set ID to restore state (without ID will be the same state for all views)
-            chartView.setId(i);
-            chartView.setListener(new ChartLayout.Listener() {
-                @Override
-                public void onInnerViewTouched() {
-                    mScrollView.requestDisallowInterceptTouchEvent(true);
-                }
-
-                @Override
-                public void onInnerViewReleased() {
-                    mScrollView.requestDisallowInterceptTouchEvent(false);
-                }
-            });
-            ChartData data = graphData.getChartData().get(i);
-            data.setDoubleYAxis(true);
-            chartView.setData(data);
-            container.addView(chartView);
-        }
-
-        // TODO persist data across activity recreations, make graphData parcable
-        graphData = new JsonGraphReader().getGraphDataFromJson(loadGraphJSONFromAsset());
-        for (int i = 0; i < graphData.getChartData().size(); i++) {
-            ChartLayout chartView = (ChartLayout) inflater.inflate(R.layout.item_chart, container, false);
-            // Need to set ID to restore state (without ID will be the same state for all views)
-            chartView.setId(i);
-            chartView.setListener(new ChartLayout.Listener() {
-                @Override
-                public void onInnerViewTouched() {
-                    mScrollView.requestDisallowInterceptTouchEvent(true);
-                }
-
-                @Override
-                public void onInnerViewReleased() {
-                    mScrollView.requestDisallowInterceptTouchEvent(false);
-                }
-            });
-            ChartData data = graphData.getChartData().get(i);
-            data.setDoubleYAxis(false);
-            chartView.setData(data);
-            container.addView(chartView);
-        }
-
-
-
-
-
 
         setSystemBarTheme(false);
     }
@@ -217,22 +159,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         outState.putBoolean(THEME, mIsLightTheme);
         super.onSaveInstanceState(outState);
-    }
-
-    public String loadGraphJSONFromAsset() {
-        String json;
-        try {
-            InputStream is = getAssets().open("chart_data.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
     }
 
     public String loadChartFromJsonData(String assetName) {
